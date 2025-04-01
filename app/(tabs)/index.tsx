bb6cfa2a0adbd6ea0762394a18baa229ba847d6b
ASSANI Younes <younesassani@gmail.com>
2025-04-01 13:45:22 +0200

    ajout MapView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ThemedText } from "@/themes/ThemedText";
+import MapView from 'react-native-maps';
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
@@ -6,7 +6,7 @@ export default function Index() {
     <View
       style={styles.container}
     >
-      <ThemedText variant="title">Recharger</ThemedText>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -14,7 +14,9 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
