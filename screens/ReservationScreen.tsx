0c9dfced21f8a70e5aef848e5a76d204ec02b298
ASSANI Younes <younesassani@gmail.com>
2025-06-02 19:12:02 +0200

    Implémentation page Réservation (pas fini)

--- screens/ReservationScreen.tsx
+++ screens/ReservationScreen.tsx
@@ -1,11 +1,60 @@
+import { useAuth } from "@/context/AuthContext";
+import { Reservation } from "@/interfaces/Reservation";
 import { ThemedText } from "@/themes/ThemedText";
-import { StyleSheet } from "react-native";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ReservationScreen() {
+  const [reservations, setReservations] = useState<Reservation[]>();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    setReservations(user?.reservations);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <ThemedText variant="title">Réservations</ThemedText>
+      <View style={{ display: "flex", gap: 8 }}>
+        <ThemedText variant="title">Réservations</ThemedText>
+        <ThemedText>
+          Vous trouverez la liste de toute les réservations passées par vous et
+          vos clients
+        </ThemedText>
+      </View>
+      <View style={{height: "36%"}}>
+        <View style={[styles.row, styles.header]}>
+          <ThemedText style={styles.cell}>Date</ThemedText>
+          <ThemedText style={styles.cell}>Début</ThemedText>
+          <ThemedText style={styles.cell}>Fin</ThemedText>
+          <ThemedText style={styles.cell}>Véhicule</ThemedText>
+          <ThemedText style={styles.cell}>Station</ThemedText>
+        </View>
+        <FlatList
+          data={reservations}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View style={styles.row}>
+              <ThemedText style={styles.cell}>
+                {moment(item.startTime).format("DD/MM/YYYY")}
+              </ThemedText>
+              <ThemedText style={styles.cell}>
+                {moment(item.startTime).format("HH:mm")}
+              </ThemedText>
+              <ThemedText style={styles.cell}>
+                {moment(item.endTime).format("HH:mm")}
+              </ThemedText>
+              <ThemedText style={styles.cell}>
+                {item.car?.model || "-"}
+              </ThemedText>
+              <ThemedText style={styles.cell}>
+                {item.station?.name || "-"}
+              </ThemedText>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -13,7 +62,20 @@ export default function ReservationScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  row: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderColor: "#ddd",
+  },
+  header: {
+    borderBottomWidth: 2,
+  },
+  cell: {
+    flex: 1,
+    fontSize: 14,
+    textAlign: "center",
   },
 });
